bf0b0f6238e9a5992c2b822a6f76321e47b0c5d2
Inbar Pe'er <inbaronet@gmail.com>
2018-03-15 00:15:43 -0400

    ugh

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "ml6.h"
 #include "display.h"
@@ -18,19 +17,12 @@ int main(int argc, char **argv) {
   edges = new_matrix(4, 4);
   transform = new_matrix(4, 4);
 
-  /* print_matrix(make_rotX(M_PI/4)); */
-  /* printf("\n"); */
-  /* print_matrix(make_rotY(M_PI/4)); */
-  /* printf("\n"); */
-  /* print_matrix(make_rotZ(M_PI/4)); */
-  /* printf("\n"); */
-
   if ( argc == 2 )
     parse_file( argv[1], transform, edges, s );
   else
     parse_file( "stdin", transform, edges, s );
 
-  //print_matrix(edges);
+  
   free_matrix( edges );
   free_matrix( transform );
 }  
